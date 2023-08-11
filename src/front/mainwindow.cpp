54cbbd34542db20239309c720049702225876bfe
Georgiy <eusebiaa@student.21-school.ru>
2023-08-11 12:20:08 +0700

    square

--- src/front/mainwindow.cpp
+++ src/front/mainwindow.cpp
@@ -1,54 +1,51 @@
 #include "mainwindow.h"
+
 #include "./ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
 
-    data_t parse_data = {0};
-    char const* pathtofile = "/Users/eusebiaa/Projects/3DViewer-in-C-QT/src/assets/test.obj";
-    // char* pathtofile = "assets/test.obj";
-    // char* pathtofile = "/home/qni/Brodichgit/3DViewer-in-C-QT/src/assets/test.obj";
+  data_t parse_data = {0};
+  char const* pathtofile =
+      "/Users/eusebiaa/Projects/3DViewer-in-C-QT/src/assets/square.obj";
+  // char* pathtofile = "assets/test.obj";
+  // char* pathtofile =
+  // "/home/qni/Brodichgit/3DViewer-in-C-QT/src/assets/test.obj";
 
-    get_parse_data(&parse_data, pathtofile);
+  get_parse_data(&parse_data, pathtofile);
 
-    polygon_t polygons; // free
-    double* vertices = NULL; // free
+  polygon_t polygons;       // free
+  double* vertices = NULL;  // free
 
-    FILE* fd;
-    fd = fopen(pathtofile, "r");
+  FILE* fd;
+  fd = fopen(pathtofile, "r");
 
-    get_vertices(fd, parse_data.count_of_vertexes, &vertices);
-    get_polygons(fd, parse_data.count_of_facets, &polygons);
+  get_vertices(fd, parse_data.amount_vertices, &vertices);
+  get_polygons(fd, parse_data.amount_polygons, &polygons);
 
-    fclose(fd);
+  fclose(fd);
 
-    printf("z %d\n", parse_data.count_of_vertexes);
-    printf("z %d\n", parse_data.count_of_facets);
+  printf("amount_vertices: %d\n", parse_data.amount_vertices);
+  printf("amount_polygons: %d\n", parse_data.amount_polygons);
 
-    ft_print_vertices(vertices, parse_data.count_of_vertexes);
-    ft_print_polygons(polygons, 6);
+  ft_print_vertices(vertices, parse_data.amount_vertices);
+  int amount_vertices_to_connect = 4;
+  ft_print_polygons(polygons, amount_vertices_to_connect);
 
+  //    glClearColor(1.0, 1.0, 1.0, 1.0);
 
+  //    if (polygons.vertexes != NULL)
+  //    ui->modelWindow->paint();
 
-//    glClearColor(1.0, 1.0, 1.0, 1.0);
-
-//    if (polygons.vertexes != NULL)
-//    ui->modelWindow->paint();
-
-//    glwidget
-//    glwidget::paintGL();
-    if (vertices)
-      free(vertices);
-    if (polygons.vertexes)
-      free(polygons.vertexes);
-
+  //    glwidget
+  //    glwidget::paintGL();
+  if (vertices) {
+    free(vertices);
+  }
+  if (polygons.vertices) {
+    free(polygons.vertices);
+  }
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
+MainWindow::~MainWindow() { delete ui; }
